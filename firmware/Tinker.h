d02eb72792380ceefa0e1f7fce865941d2aa9f05
jscottb <scottbeasley@gmail.com>
2016-01-14 09:06:39 -0500

    Added servo struct and use counter to Tinker.h

--- firmware/Tinker.h
+++ firmware/Tinker.h
@@ -1,3 +1,12 @@
+/* Globals -------------------------------------------------------------------*/
+// Allow for at least 9 servos.
+struct usrservos {
+   Servo servo;
+   int pin;
+} userservos[9];
+
+int servosinuse = 0;
+
 /* Function prototypes -------------------------------------------------------*/
 int tinkerDigitalRead(String pin);
 int tinkerDigitalWrite(String command);
